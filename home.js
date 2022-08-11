d602816f0e82319c321ae45c65f750701b895192
Nitish <nitishkathuria33@gamil.com>
2022-08-11 18:14:58 +0530

    Meals Details Section Created

--- home.js
+++ home.js
@@ -2,7 +2,31 @@
 const searchBar = document.querySelector("input");
 const mealContainer = document.getElementsByClassName("content");
 const result = document.getElementsByClassName("result");
+const modal = document.getElementsByClassName("get-details");
+const image = document.querySelector(".get-details .meals-image .image");
+const reciepe = document.querySelector(".get-details .reciepe p");
+const dishName = document.querySelector(".get-details .dish-name");
+const watchVedio = document.querySelectorAll(
+  ".get-details .container .btn-container button"
+);
 // Creating functions
+async function getMealDetails(mealid) {
+  const url = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + mealid;
+  const response = await fetch(url);
+  const data = await response.json();
+  const [meal] = data.meals;
+  image.style.backgroundImage = `url(${meal.strMealThumb})`;
+  reciepe.innerText = meal.strInstructions;
+  dishName.innerText = meal.strMeal;
+  // adding event listener to watch-vedio button
+  watchVedio[0].addEventListener("click", () => {
+    window.open(meal.strYoutube);
+  });
+  // adding event listener to back-button
+  watchVedio[1].addEventListener("click", () => {
+    modal[0].style.visibility = "hidden";
+  });
+}
 async function displayMeal(dishName) {
   const url =
     "https://www.themealdb.com/api/json/v1/1/search.php?s=" + dishName;
@@ -22,11 +46,20 @@ async function displayMeal(dishName) {
       <img src=${meal.strMealThumb} alt="meal" />
     </div>
     <div class="button-container">
-      <button data-id=${meal.idMeal} type="button">Get Details</button>
+      <button data-id=${meal.idMeal}  type="button">Get Details</button>
       <ion-icon data-id=${meal.idMeal} name="bookmarks-outline"></ion-icon>
     </div>
   </div>`;
     mealContainer[0].insertAdjacentHTML("beforeend", html);
+  }
+  // Handling event listeners on all buttons to get details of the meals
+  const getDetailButton = document.querySelectorAll(".button-container button");
+  for (let btn of getDetailButton) {
+    btn.addEventListener("click", (e) => {
+      modal[0].style.visibility = "visible";
+      const mealid = e.target.getAttribute("data-id");
+      getMealDetails(mealid);
+    });
   }
 }
 
